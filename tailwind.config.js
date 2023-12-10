c311da9571bb4836d16b8f797abfa2eeae8dbf36
Piero Liberato <54274115+Pierou20181h057@users.noreply.github.com>
2023-12-10 13:56:19 -0500

    Add: Card design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        'card': '0 25px 50px -15px rgb(0 0 0 / 0.25);'
+      }
+    },
   },
   plugins: [],
 }
