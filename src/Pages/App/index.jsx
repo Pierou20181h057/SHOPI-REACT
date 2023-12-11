e1c4fdfb9b137d79b790a1100eaab8b0b8c77291
Piero Liberato <54274115+Pierou20181h057@users.noreply.github.com>
2023-12-10 19:29:08 -0500

    Add: Implement global context

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,5 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
-
+import { ShoppingCartProvider } from '../../Context'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -26,10 +26,12 @@ const App= () => {
   
 
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-      <NavBar/>
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <AppRoutes/>
+        <NavBar/>
+      </BrowserRouter>
+    </ShoppingCartProvider>
   )
 }
 
