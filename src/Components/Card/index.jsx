df722101b633f517becf7fd7ef04574139a3488f
Piero Liberato <54274115+Pierou20181h057@users.noreply.github.com>
2023-12-10 19:12:36 -0500

    Add: Implemente consumption of API

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,16 +1,16 @@
-const Card = () => {
+const Card = (data) => {
     return(
         <div className="dg-white cursor-pointer w-56 h-60 rounded-lg shadow-card">
             <figure className="relative mb-2 w-full h-5/6">
-                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5 font-medium">Electronics</span>
-                <img className="w-full h-full object-cover rounded-lg" src="https://images.pexels.com/photos/17126918/pexels-photo-17126918/free-photo-of-mujer-ordenador-portatil-sentado-mesa.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="headphones"></img>
+                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5 font-medium">{data.data.category.name}</span>
+                <img className="w-full h-full object-cover rounded-lg" src={data.data.images[0]} alt={data.data.title}></img>
                 <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
                     +
                 </div>
             </figure>
             <p className="flex justify-between items-center">
-                <span className="text-sm font-light ml-2">Headphones</span>
-                <span className="text-lg font-medium mr-2">$300</span>
+                <span className="text-sm font-light ml-2">{data.data.title}</span>
+                <span className="text-lg font-medium mr-2">${data.data.price}</span>
             </p>
         </div>
     )
