8b41a4a1c41729c4a1f32080cbe4480115775c7f
Piero Liberato <54274115+Pierou20181h057@users.noreply.github.com>
2023-12-10 19:39:26 -0500

    Add: Counter of products

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,10 +1,15 @@
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
 const Card = (data) => {
+    const context = useContext(ShoppingCartContext)
     return(
         <div className="dg-white cursor-pointer w-56 h-60 rounded-lg shadow-card">
             <figure className="relative mb-2 w-full h-5/6">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5 font-medium">{data.data.category.name}</span>
                 <img className="w-full h-full object-cover rounded-lg" src={data.data.images[0]} alt={data.data.title}></img>
-                <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
+                <div 
+                    className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
+                    onClick={()=> context.setCount(context.count + 1)}>
                     +
                 </div>
             </figure>
