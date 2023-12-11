a0dc10156edd0538240ead07b0c8637111b6c7d4
Piero Liberato <54274115+Pierou20181h057@users.noreply.github.com>
2023-12-11 07:15:59 -0500

    Add: Implement Add products

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -8,6 +8,11 @@ const Card = (data) => {
         context.openProductDetail()
         context.setProductToShow(productDetail)
     }
+    const addProductsToCart = (productData) =>{
+        context.setCount(context.count + 1)
+        context.setCartProducts([...context.cartProducts, productData])
+        console.log('Cart:', context.cartProducts)
+    }
     return(
         <div 
             className="dg-white cursor-pointer w-56 h-60 rounded-lg shadow-card"
@@ -17,7 +22,7 @@ const Card = (data) => {
                 <img className="w-full h-full object-cover rounded-lg" src={data.data.images[0]} alt={data.data.title}></img>
                 <div 
                     className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-                    onClick={()=> context.setCount(context.count + 1)}>
+                    onClick={()=>addProductsToCart(data.data)}>
                     <PlusIcon className="h-6 w-6 text-black"/>
                 </div>
             </figure>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -13,6 +13,8 @@ export const ShoppingCartProvider = ({children}) => {
 
     //Product Detail Show product
     const [productToShow, setProductToShow] = useState({})
+    //Shopping Cart Add products to cart
+    const [cartProducts, setCartProducts] = useState([])
 
     return(
         <ShoppingCartContext.Provider value={{
@@ -22,7 +24,9 @@ export const ShoppingCartProvider = ({children}) => {
             closeProductDetail,
             isProductDetailOpen,
             productToShow,
-            setProductToShow
+            setProductToShow,
+            setCartProducts,
+            cartProducts
         }}>
             {children}
         </ShoppingCartContext.Provider>
